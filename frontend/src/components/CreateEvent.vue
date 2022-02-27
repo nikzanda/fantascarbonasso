be8b3c9c8c1cd0a90b893e3b43e07f970ed72eb5
nikzanda <nikzanda@hotmail.it>
2022-02-27 18:39:31 +0100

    form creazione evento

--- frontend/src/components/CreateEvent.vue
+++ frontend/src/components/CreateEvent.vue
@@ -1,29 +1,36 @@
 <template>
   <v-card class="mx-auto">
     <v-form v-model="valid" ref="form">
-      <!-- Team -->
-      <v-autocomplete
-        v-model="form.team_id"
-        :items="teams"
-        :rules="[(v) => !!v || 'Team obbligatorio']"
-        label="Team"
-        required
-      />
+      <v-card-text>
+        <!-- Team -->
+        <v-autocomplete
+          v-model="form.team_id"
+          :items="teams"
+          :rules="[(v) => !!v || 'Team obbligatorio']"
+          label="Team"
+          item-text="name"
+          item-value="id"
+          required
+        >
+        </v-autocomplete>
 
-      <!-- Category -->
-      <v-autocomplete
-        v-model="form.category_id"
-        :items="categories"
-        :rules="[(v) => !!v || 'Categoria obbligatoria']"
-        label="Categoria"
-        required
-      />
+        <!-- Category -->
+        <v-autocomplete
+          v-model="form.category_id"
+          :items="categories"
+          :rules="[(v) => !!v || 'Categoria obbligatoria']"
+          label="Categoria"
+          item-text="description"
+          item-value="id"
+          required
+        />
 
-      <!-- Created by Team Leader -->
-      <v-checkbox
-        v-model="form.created_by_leader"
-        label="Creato dal Team Leader"
-      />
+        <!-- Created by Team Leader -->
+        <v-checkbox
+          v-model="form.created_by_leader"
+          label="Creato dal Team Leader"
+        />
+      </v-card-text>
 
       <v-card-actions>
         <v-btn :disabled="!valid" color="success" class="mr-4" @click="submit">
@@ -49,12 +56,31 @@ export default {
       created_by_leader: false,
     },
   }),
+  created() {
+    this.getTeams();
+    this.getCategories();
+  },
   methods: {
+    getTeams() {
+      this.$axios("/team")
+        .then(({ data }) => (this.teams = data))
+        .catch(console.error);
+    },
+    getCategories() {
+      this.$axios("/category")
+        .then(({ data }) => (this.categories = data))
+        .catch(console.error);
+    },
     reset() {
       this.$refs.form.reset();
     },
     submit() {
-      this.$refs.form.validate();
+      if (!this.$refs.form.validate()) return;
+
+      this.$axios
+        .post("/event", this.form)
+        .then(console.log)
+        .catch(console.error);
     },
   },
 };
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
-Vue.config.productionTip = false
+Vue.use(VueAxios, axios);
+Vue.config.productionTip = false;
+
+Vue.prototype.$axios = axios.create({
+  baseURL: process.env.VUE_APP_API_BASE_URL,
+});
+
+console.warn(process.env.VUE_APP_API_BASE_URL);
 
 new Vue({
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
