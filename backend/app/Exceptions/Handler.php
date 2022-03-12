c351b18e63f5b039bf654885434074e3a4b203f6
nikzanda <nikzanda@hotmail.it>
2022-03-12 18:59:39 +0100

    fix

--- backend/app/Exceptions/Handler.php
+++ backend/app/Exceptions/Handler.php
@@ -3,6 +3,10 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Response;
 use Throwable;
 
 class Handler extends ExceptionHandler
