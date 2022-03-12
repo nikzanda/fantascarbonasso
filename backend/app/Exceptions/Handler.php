743a5a18f3c2d2631ade53113d6ac7831816859b
nikzanda <nikzanda@hotmail.it>
2022-03-12 18:59:03 +0100

    fix Handler per risposta in JSON

--- backend/app/Exceptions/Handler.php
+++ backend/app/Exceptions/Handler.php
@@ -74,4 +74,39 @@ class Handler extends ExceptionHandler
 
         return parent::render($request, $exception);
     }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($request->wantsJson()) {
+            if ($exception instanceof ModelNotFoundException)
+                return response()->json(['message' => __('custom.not_found')], Response::HTTP_NOT_FOUND);
+
+            if ($exception instanceof QueryException) {
+                $message = $exception->errorInfo[2] ?? __('custom.default');
+                $errno = 500;
+
+                switch ($exception->errorInfo[1]) {
+                    case 1062:
+                        $message = __('custom.duplicate');
+                        $errno = Response::HTTP_UNPROCESSABLE_ENTITY;
+                        break;
+                }
+
+                return response()->json(['message' => $message], $errno);
+            }
+
+            if ($exception instanceof ValidationException) {
+                $message = '';
+
+                foreach ($exception->errors() as $val)
+                    $message .= $val[0] . ' ';
+
+                return response()->json(['message' => trim($message)], Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+
+            return response()->json(['message' => $exception->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return parent::render($request, $exception);
+    }
 }
